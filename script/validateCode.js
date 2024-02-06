38f33bf89f6d836dccc5ac3dae44bfa6b6a2b4ba
LEROUX Antonin <antonin.leroux@univ-rouen.fr>
2024-02-06 10:55:34 +0100

    Added code check script 	new file:   script/validateCode.js

---
+++ script/validateCode.js
@@ -0,0 +1,12 @@
+
+function isCodeCorrect(){
+
+    var usrPrompt = document.getElementsByClassName("btn")[0];
+
+    if (usrPrompt == correctAnswer){
+        return true;
+    }else{
+        return false;
+    }
+
+}
