46a4ce69b37e5be7f3b8f565d44833b1491ab87b
SamuelPrieur <sprieurpro@gmail.com>
2024-02-07 17:30:44 +0100

    modified:   index.html 	modified:   script/reset.js

--- script/reset.js
+++ script/reset.js
@@ -1,3 +1,5 @@
-document.getElementsByTagName("button")[0].addEventListener("click",function(){
+document
+  .getElementsByTagName("button")[0]
+  .addEventListener("click", function () {
     document.getElementsByTagName("textarea")[0].value = "";
-})
+  });
