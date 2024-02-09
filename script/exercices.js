8f74e89e0d1e9f80df82d8c29e7dbf2666a07dda
LEROUX Antonin <antonin.leroux@univ-rouen.fr>
2024-02-09 15:55:13 +0100

    Added tutorial and question on each exercice 	modified:   index.html 	modified:   script/exercices.js

--- script/exercices.js
+++ script/exercices.js
@@ -12,7 +12,15 @@ let display = document.getElementById("display");
 
 let notif = document.getElementById("notif");
 
+//Récupération des zone de consignes
+
+let lecon = document.getElementById("lecon");
+let consigne = document.getElementById("consigne")
+
 document.title = "Tailwind Tutorial | Exercice 1";
+
+lecon.innerHTML = "Tailwind CSS permet de styliser rapidement et efficacement votre interface en utilisant des classes prédéfinies. La classe text-gray-500 est utilisée pour définir la couleur du texte en gris. Vous pouvez facilement changer la couleur du texte en utilisant d'autres classes de couleur disponibles dans la documentation.";
+consigne.innerHTML = "Dans le code suivant, changez la classe du texte pour obtenir une couleur différente. Utilisez l'une des classes de couleur de texte disponibles dans Tailwind CSS. Assurez-vous de comprendre comment les classes de couleur fonctionnent."
 
 reset.addEventListener("click",function(){
 
@@ -45,6 +53,9 @@ function exo2(){
     document.title = "Tailwind Tutorial | Exercice 2";
     notif.innerHTML = "";
 
+    lecon.innerHTML = "Tailwind CSS propose des classes utilitaires pour ajuster la taille du texte. La classe text-xl est utilisée pour définir la taille du texte comme extra-large. Vous pouvez expérimenter avec différentes classes de taille de texte pour atteindre le rendu souhaité.";
+    consigne.innerHTML = "Modifiez la classe de taille du texte dans le code ci-dessous pour obtenir une taille différente. Utilisez l'une des classes de taille de texte disponibles dans Tailwind CSS. Assurez-vous de comprendre comment ces classes fonctionnent.";
+
     reset.addEventListener("click",function(){
 
         code.value = '<p class="text-xl">Ce texte a actuellement une taille extra-large. Modifiez la classe pour ajuster la taille en large.</p>'
@@ -76,6 +87,9 @@ function exo3(){
 
     document.title = "Tailwind Tutorial | Exercice 3";
     notif.innerHTML = "";
+
+    lecon.innerHTML = "Dans Tailwind CSS, vous pouvez styliser la couleur de fond d'un élément en utilisant des classes telles que bg-blue-300. De plus, les marges peuvent être ajustées à l'aide de classes comme m-4 pour une marge de 1.0rem.";
+    consigne.innerHTML = "Modifiez la couleur de fond et la marge de la boîte dans le code ci-dessous pour obtenir un résultat différent. Utilisez les classes de couleur de fond et de marge disponibles dans Tailwind CSS. Assurez-vous de comprendre comment ces classes influencent le style de l'élément.";
 
     reset.addEventListener("click",function(){
 
