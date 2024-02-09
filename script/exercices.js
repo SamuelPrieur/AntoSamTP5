c288c1f1373d6fdb29dbf902d46c555e630d819f
LEROUX Antonin <antonin.leroux@univ-rouen.fr>
2024-02-09 15:21:51 +0100

    Added exercice 1 and exercice 2, UI changed     modified:   index.html 	deleted:    script/ex1.js 	new file:   script/exercices.js

---
+++ script/exercices.js
@@ -0,0 +1,73 @@
+//Récuperation des boutons
+
+let reset = document.getElementById("reset");
+let submit = document.getElementById("submit");
+
+//Récupération des zones de code et d'affichage
+
+let code = document.getElementById("code");
+let display = document.getElementById("display");
+
+//Récupératon de la zone de notification
+
+let notif = document.getElementById("notif");
+
+document.title = "Tailwind Tutorial | Exercice 1";
+
+reset.addEventListener("click",function(){
+
+    code.value = "<h1 class='text-gray-500'>Ce titre est actuellement gris, modifiez la classe pour le mettre en rouge</h1>"
+    display.innerHTML = "<h1 class='text-gray-500'>Ce titre est actuellement gris, modifiez la classe pour le mettre en rouge</h1>"
+
+})
+
+let correctAnswer = "<h1 class='text-red-500'>Ce titre est actuellement gris, modifiez la classe pour le mettre en rouge</h1>"
+
+code.value = "<h1 class='text-gray-500'>Ce titre est actuellement gris, modifiez la classe pour le mettre en rouge</h1>"
+display.innerHTML = "<h1 class='text-gray-500'>Ce titre est actuellement gris, modifiez la classe pour le mettre en rouge</h1>"
+
+submit.addEventListener("click",function(){
+
+    if(code.value == correctAnswer){
+
+        notif.innerHTML = '<div role="alert" class="alert alert-success"><svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg><div><h3 class="font-bold">Bravo !</h3><div class="text-xs">Vous avez trouvé la bonne réponse.</div></div><button onclick="exo2()" class="btn btn-sm">Suivant</button></div>'
+        console.log("true");
+    }else{
+
+        notif.innerHTML = '<div role="alert" class="alert alert-error"><svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg><span>Erreur ! Le code entré est invalide.</span></div>'
+        console.log("false");
+    }
+
+})
+
+function exo2(){
+
+    document.title = "Tailwind Tutorial | Exercice 2";
+    notif.innerHTML = "";
+
+    reset.addEventListener("click",function(){
+
+        code.value = '<p class="text-xl">Ce texte a actuellement une taille extra-large. Modifiez la classe pour ajuster la taille du texte.</p>'
+        display.innerHTML = '<p class="text-xl">Ce texte a actuellement une taille extra-large. Modifiez la classe pour ajuster la taille du texte.</p>'
+    })
+
+    let correctAnswer = '<p class="text-lg">Ce texte a actuellement une taille extra-large. Modifiez la classe pour ajuster la taille du texte.</p>'
+
+    code.value = '<p class="text-xl">Ce texte a actuellement une taille extra-large. Modifiez la classe pour ajuster la taille du texte.</p>'
+    display.innerHTML = '<p class="text-xl">Ce texte a actuellement une taille extra-large. Modifiez la classe pour ajuster la taille du texte.</p>'
+
+    submit.addEventListener("click",function(){
+
+        if(code.value == correctAnswer){
+    
+            notif.innerHTML = '<div role="alert" class="alert alert-success"><svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg><div><h3 class="font-bold">Bravo !</h3><div class="text-xs">Vous avez trouvé la bonne réponse.</div></div><button onclick="exo3()" class="btn btn-sm">Suivant</button></div>'
+            console.log("true");
+        }else{
+    
+            notif.innerHTML = '<div role="alert" class="alert alert-error"><svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg><span>Erreur ! Le code entré est invalide.</span></div>'
+            console.log("false");
+        }
+    
+    })
+
+}
