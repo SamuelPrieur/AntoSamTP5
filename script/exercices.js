542d07aed0ea0614d749369cc847e102afe95f06
LEROUX Antonin <antonin.leroux@univ-rouen.fr>
2024-02-09 15:32:50 +0100

    Fixed exercice 2 question unclear, WIP exercice 3 	modified:   script/exercices.js

--- script/exercices.js
+++ script/exercices.js
@@ -47,14 +47,46 @@ function exo2(){
 
     reset.addEventListener("click",function(){
 
-        code.value = '<p class="text-xl">Ce texte a actuellement une taille extra-large. Modifiez la classe pour ajuster la taille du texte.</p>'
-        display.innerHTML = '<p class="text-xl">Ce texte a actuellement une taille extra-large. Modifiez la classe pour ajuster la taille du texte.</p>'
+        code.value = '<p class="text-xl">Ce texte a actuellement une taille extra-large. Modifiez la classe pour ajuster la taille en large.</p>'
+        display.innerHTML = '<p class="text-xl">Ce texte a actuellement une taille extra-large. Modifiez la classe pour ajuster la taille en large.</p>'
     })
 
-    let correctAnswer = '<p class="text-lg">Ce texte a actuellement une taille extra-large. Modifiez la classe pour ajuster la taille du texte.</p>'
+    let correctAnswer = '<p class="text-lg">Ce texte a actuellement une taille extra-large. Modifiez la classe pour ajuster la taille en large.</p>'
 
-    code.value = '<p class="text-xl">Ce texte a actuellement une taille extra-large. Modifiez la classe pour ajuster la taille du texte.</p>'
-    display.innerHTML = '<p class="text-xl">Ce texte a actuellement une taille extra-large. Modifiez la classe pour ajuster la taille du texte.</p>'
+    code.value = '<p class="text-xl">Ce texte a actuellement une taille extra-large. Modifiez la classe pour ajuster la taille en large.</p>'
+    display.innerHTML = '<p class="text-xl">Ce texte a actuellement une taille extra-large. Modifiez la classe pour ajuster la taille en large.</p>'
+
+    submit.addEventListener("click",function(){
+
+        if(code.value == correctAnswer){
+    
+            notif.innerHTML = '<div role="alert" class="alert alert-success"><svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg><div><h3 class="font-bold">Bravo !</h3><div class="text-xs">Vous avez trouvé la bonne réponse.</div></div><button onclick="exo3()" class="btn btn-sm">Suivant</button></div>'
+            console.log("true");
+        }else{
+    
+            notif.innerHTML = '<div role="alert" class="alert alert-error"><svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg><span>Erreur ! Le code entré est invalide.</span></div>'
+            console.log("false");
+        }
+    
+    })
+
+}
+
+function exo3(){
+
+    document.title = "Tailwind Tutorial | Exercice 3";
+    notif.innerHTML = "";
+
+    reset.addEventListener("click",function(){
+
+        code.value = '<div class="bg-gray-200 m-6 p-4"><p>Ceci est un contenu dans une boîte avec une couleur de fond grise et une marge de 1.5rem.</p></div>'
+        display.innerHTML = '<div class="bg-gray-200 m-6 p-4"><p>Ceci est un contenu dans une boîte avec une couleur de fond grise et une marge de 1.5rem.</p></div>'
+    })
+
+    let correctAnswer = '<div class="bg-green-400 m-8 p-4"><p>Ceci est un contenu dans une boîte avec une couleur de fond verte et une marge de 2.0rem.</p></div>'
+
+    code.value = '<div class="bg-gray-200 m-6 p-4"><p>Ceci est un contenu dans une boîte avec une couleur de fond grise et une marge de 1.5rem.</p></div>'
+    display.innerHTML = '<div class="bg-gray-200 m-6 p-4"><p>Ceci est un contenu dans une boîte avec une couleur de fond grise et une marge de 1.5rem.</p></div>'
 
     submit.addEventListener("click",function(){
 
